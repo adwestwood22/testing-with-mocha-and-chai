b0eb0603b1cd2d30160dc0e981526aefc415b46b
Westwood <westwa65@009L60ADADMMAC.local>
2016-09-02 16:03:59 -0700

    updated test description

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 describe('A basic test', function(){
 
-	it('should pass when everything is ok', function(){
+	it('check that <true condition> is true', function(){
 
 		expect(true).to.be.true;
 
