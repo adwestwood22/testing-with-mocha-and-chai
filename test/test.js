73c3b1d73e9a51fa83c6c14ac4e4dff1966b4384
Westwood <westwa65@009L60ADADMMAC.local>
2016-09-02 18:11:38 -0700

    added FaaService testing with promises

--- test/test.js
+++ test/test.js
@@ -1,21 +1,28 @@
 'use strict';
 
+//requires
 var chai = require('chai');
 var expect = require('chai').expect;
 var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
 
+//local reqires
 var FaaService = require('../services/FaaService');
 
+//build
+chai.use(chaiAsPromised);
 
+
+//test scenarios
 describe('A basic test of the FaaService using promises', function(){
 
-
+	//scenario variables
 	var faaService;
 	var airportCode;
 
+	//test scenario context #1
 	context('initialization', function(){
 
+		//context #1 assertion #1
 		it('check that the FaaService can be instantiated', function(){
 
 			faaService = new FaaService();
@@ -25,9 +32,10 @@ describe('A basic test of the FaaService using promises', function(){
 
 	})
 
-
+	//test scenario context #2
 	context('using a valid airport code', function(){
 
+		//context #2 assertion #1
 		it('check getAirportStatus works on FaaService', function(){
 
 			//using chai-as-promised for the rest call
